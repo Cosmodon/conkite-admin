c7ace898d5f8d9733b6496aa66802d8c86485364
Shaun Underwood <su@sunderwood.co.uk>
2019-12-11 21:46:49 +0100

    removed actions from payment table

--- src/Payments.tsx
+++ src/Payments.tsx
@@ -65,58 +65,11 @@ class Payments extends React.Component<{
 							editable: "always",
 						},
 					]}
-					editable={{
-						isEditable: (rowData: any) => false,
-						isDeletable: (rowData: any) => false,
-						onRowUpdate: (newData, oldData) =>
-							new Promise(async (resolve, reject) => {
-								const corrlinks_id = oldData.corrlinks_id;
-								try {
-									validate(newData);
-									await this.props.store.app.updateUser({ corrlinks_id, user: newData });
-									resolve();
-								} catch (e) {
-									reject();
-								}
-							}),
-						onRowDelete: oldData =>
-							new Promise(async (resolve, reject) => {
-								try {
-									await this.props.store.app.deleteUser({ corrlinks_id: oldData.corrlinks_id });
-									resolve();
-								} catch (e) {
-									console.log(e);
-									alert(e);
-									reject();
-								}
-							}),
-						// onRowAdd: newData =>
-						// 	new Promise(async (resolve, reject) => {
-						// 		try {
-						// 			validate(newData);
-						// 			await this.props.store.app.addUser({ user: newData });
-						// 			resolve();
-						// 		} catch (e) {
-						// 			console.log(e);
-						// 			alert(e)
-						// 			reject();
-						// 		}
-						// 	}),
-					}}
-
 				/>
 
 			</React.Fragment>
 		);
 	}
-}
-function validate(a) {
-	if (!a.corrlinks_id || ('' + a.corrlinks_id).length < 8) throw new Error('corrlinks_id must be 8 digits long');
-	if (!a.name || a.name.lengt < 1) throw new Error('name required');
-	if (!a.date_subscription_ends || !new Date(a.date_subscription_ends).valueOf()) throw new Error('date_subscription_ends must be a valid date');
-	console.log({ a });
-	if (!a.status || statuses.filter(b => b.value === a.status).length === 0) throw new Error(`status must be one of [${statuses.map(a => `${a.value}`).join(', ')}]`);
-	return true;
 }
 
 export default Payments;
