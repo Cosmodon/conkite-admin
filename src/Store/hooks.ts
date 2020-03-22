78bd6bdf4828edd1b4805baa8baa481acf598625
Cosmo DeHaan <cosmodon411974@gmail.com>
2020-03-22 14:24:07 +0100

    remove old imports

--- src/Store/hooks.ts
+++ src/Store/hooks.ts
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { MobXProviderContext } from "mobx-react";
 
 export const useStore = () => React.useContext(MobXProviderContext).store.app;
