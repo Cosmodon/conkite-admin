57687c657a691e05c854c92d856f2a0d9c75a5fa
Shaun Underwood <su@sunderwood.co.uk>
2022-12-03 14:11:41 +0100

    Updates

---
+++ precache-manifest.f9999d2445774b51d103134c18b24e82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3458f24d7662d73e9614076fff192bd9",
+    "url": "/conkite-admin/index.html"
+  },
+  {
+    "revision": "51fa532cd5c979ce41a3",
+    "url": "/conkite-admin/static/js/2.b41ab3c7.chunk.js"
+  },
+  {
+    "revision": "5eda63a998ee61c27853410febccb63c",
+    "url": "/conkite-admin/static/js/2.b41ab3c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab9bce5b12f9271c0af",
+    "url": "/conkite-admin/static/js/main.0ecc3d15.chunk.js"
+  },
+  {
+    "revision": "51453a3b7a744e988da7",
+    "url": "/conkite-admin/static/js/runtime-main.bcc90c6e.js"
+  }
+]);
--- src/Store/hooks.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { MobXProviderContext } from "mobx-react";
-
-export const useStore = () => React.useContext(MobXProviderContext).store.app;
-export const useUserStore = () => React.useContext(MobXProviderContext).store.user;
-export const usePhonebookStore = () => React.useContext(MobXProviderContext).store.phonebook;
